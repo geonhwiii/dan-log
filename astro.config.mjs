fd62d6f0ac5cf7d6ddc14b9481ee8b96ad3d96ce
Dan <44767362+geonhwiii@users.noreply.github.com>
2025-01-19 15:19:22 +0900

    feat: update biome & apply theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  site: 'https://danlog.vercel.app',
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+	output: 'static',
+	site: 'https://danlog.vercel.app',
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
+	integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+	markdown: {
+		shikiConfig: {
+			theme: 'night-owl',
+		},
+	},
 });
--- src/consts.ts
+++ src/consts.ts
@@ -2,62 +2,62 @@ import type { Site, Page, Links, Socials } from '@types';
 
 // Global
 export const SITE: Site = {
-  TITLE: '단로그',
-  DESCRIPTION: '프론트엔드 개발자 Dan의 개발 일상 블로그입니다.',
-  AUTHOR: 'Dan',
+	TITLE: '단로그',
+	DESCRIPTION: '프론트엔드 개발자 Dan의 개발 일상 블로그입니다.',
+	AUTHOR: 'Dan',
 };
 
 // Work Page
 export const WORK: Page = {
-  TITLE: '프로필',
-  DESCRIPTION: '프로필 및 경력',
+	TITLE: '프로필',
+	DESCRIPTION: '프로필 및 경력',
 };
 
 // Blog Page
 export const BLOG: Page = {
-  TITLE: '블로그',
-  DESCRIPTION: '단의 블로그 글 목록',
+	TITLE: '블로그',
+	DESCRIPTION: '단의 블로그 글 목록',
 };
 
 // Search Page
 export const SEARCH: Page = {
-  TITLE: '검색',
-  DESCRIPTION: '단로그의 글 목록 검색',
+	TITLE: '검색',
+	DESCRIPTION: '단로그의 글 목록 검색',
 };
 
 // Links
 export const LINKS: Links = [
-  {
-    TEXT: '홈',
-    HREF: '/',
-  },
-  {
-    TEXT: '블로그',
-    HREF: '/blog',
-  },
-  {
-    TEXT: '프로필',
-    HREF: '/work',
-  },
+	{
+		TEXT: '홈',
+		HREF: '/',
+	},
+	{
+		TEXT: '블로그',
+		HREF: '/blog',
+	},
+	{
+		TEXT: '프로필',
+		HREF: '/work',
+	},
 ];
 
 export const SOCIALS: Socials = [
-  {
-    NAME: 'Email',
-    ICON: 'email',
-    TEXT: 'gunw.dan@gmail.com',
-    HREF: 'mailto:gunw.dan@gmail.com',
-  },
-  {
-    NAME: 'Github',
-    ICON: 'github',
-    TEXT: 'geonhwiii',
-    HREF: 'https://github.com/geonhwiii',
-  },
-  {
-    NAME: 'LinkedIn',
-    ICON: 'linkedin',
-    TEXT: 'gunwww',
-    HREF: 'https://www.linkedin.com/in/gunwww/',
-  },
+	{
+		NAME: 'Email',
+		ICON: 'email',
+		TEXT: 'gunw.dan@gmail.com',
+		HREF: 'mailto:gunw.dan@gmail.com',
+	},
+	{
+		NAME: 'Github',
+		ICON: 'github',
+		TEXT: 'geonhwiii',
+		HREF: 'https://github.com/geonhwiii',
+	},
+	{
+		NAME: 'LinkedIn',
+		ICON: 'linkedin',
+		TEXT: 'gunwww',
+		HREF: 'https://www.linkedin.com/in/gunwww/',
+	},
 ];
